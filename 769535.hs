65a8c0208ddc8441aaf41e7c5faa090641416e3b
Matchoo95 <mattmatth123@googlemail.com>
2017-03-13 22:55:36 +0000

    Q6. plus attempt at Q5 + demos so far

--- 769535.hs
+++ 769535.hs
@@ -1,12 +1,21 @@
 import Data.List
+import Text.Printf
+import System.IO
+import Control.Exception
 
+--
+-- MATHFUN
+-- UP769535
+--
+
+--
+-- Types
+--
 type Title = String
 type Director = String
 type Year = Int
 type Fans = [String]
-
 type Film = (Title, Director, Year, Fans)
-type Database = [Film] -- TODO check if i need this at the end and remove if not
 
 testDatabase :: [Film]
 testDatabase = [
@@ -38,34 +47,93 @@ testDatabase = [
   ]
 
 
--- Functional code
+  --
+  --
+  --  Your functional code goes here
+  --
+  --
 
---1
-addFilm :: String -> String -> Int -> [Film] -> [Film]
+--1 add a new film to the database
+addFilm :: Title -> Director -> Year -> [Film] -> [Film]
 addFilm title director year film = (title, director, year, []) : film
 
---2
+--2 give all films in the database
 filmsAsString :: [Film] -> String
 filmsAsString [] = ""
 filmsAsString  ((title, director, year, fans):xs) = title ++ " by " ++ director ++
                                                     ", released " ++  (show year)
                                                     ++ ".\n" ++ filmsAsString xs
 
---3
+--3 give all the films that were released after a particular year (not including the given year)
 displayFilmsAfterYear :: Year -> [Film] -> String
-displayFilmsAfterYear year database = filmsAsString (filter(checkFilm year) database)
+displayFilmsAfterYear year database = filmsAsString(filter(checkFilm year) database)
 
 checkFilm :: Year -> Film -> Bool
-checkFilm year (t, d, y, f)
-          | year <= y = True
+checkFilm y (title, director, year, fans)
+          | y < year = True
           | otherwise = False
 
---4
-filmsByFan fanName = filter (checkByFan fanName)
+--4 give all films that a particular user is a fan of
+filmsByFan :: String -> [Film] -> String
+filmsByFan fanName database = filmsAsStringFans(filter(isFan fanName) database)
 
-checkByFan :: String -> Film -> Bool
-checkByFan fanName (title, director, year, fans)
+isFan :: String -> Film -> Bool
+isFan fanName (title, director, year, fans)
           | elem fanName fans = True
           | otherwise = False
 
---5
+--5 give all the fans of a particular film
+{--
+fansOfFilm :: [Film] -> Fans -> String
+fansOfFilm filmName fans = fansAsString (filter(isFilm filmName) fans)
+
+isFilm :: Title -> Film -> Bool
+isFilm filmName (title, director, year, fans)
+          | filmName == title = True
+          | otherwise = False
+
+fansAsString :: Fans -> String
+fansAsString [] = ""
+fansAsString  (x:xs) = (show x) ++ fansAsString ++ xs
+                                                    --}
+filmsAsStringFans :: [Film] -> String
+filmsAsStringFans [] = ""
+filmsAsStringFans  ((title, director, year, fans):xs) = title ++ " by " ++ director ++
+                                                    ", released " ++  (show year)
+                                                    ++ ".\nThe fans for this film are: "
+                                                    ++ (show fans) ++ ".\n\n" ++ filmsAsStringFans xs
+
+
+--6 allow a user to say they are a fan of a particular film
+addFan :: String -> Title -> [Film] -> [Film]
+addFan fan filmTitle [] = []
+addFan fan filmTitle ((title, director, year, fans) : xs)
+          | (filmTitle == title) && not(isFan fan (title, director, year, fans))
+              = (title, director, year, fan : fans) : addFan fan filmTitle xs
+          | otherwise = (title, director, year, fans) : addFan fan filmTitle xs
+
+-- Demo function to test basic functionality (without persistence - i.e.
+-- testDatabase doesn't change and nothing is saved/loaded to/from file).
+
+demo :: Int -> IO ()
+demo 1 = putStrLn(filmsAsString(addFilm "Alien: Covenant" "Ridley Scott" 2017 testDatabase))
+--demo 1  = putStrLn all films after adding 2017 film "Alien: Covenant"
+--                   by "Ridley Scott" to testDatabase
+demo 2 = putStrLn(filmsAsString testDatabase)
+--demo 3  = putStrLn all films released after 2008
+demo 3 = putStrLn(displayFilmsAfterYear 2008 testDatabase)
+--demo 4  = putStrLn all films that "Liz" is a fan of
+demo 4 = putStrLn(filmsByFan "Liz" testDatabase)
+--demo 5  = putStrLn all fans of "Jaws"
+--demo 6  = putStrLn all films after "Liz" says she becomes fan of "The Fly"
+--demo 66 = putStrLn all films after "Liz" says she becomes fan of "Avatar"
+demo 6 = putStrLn(filmsAsStringFans(addFan "Liz" "The Fly" testDatabase))
+demo 66 = putStrLn(filmsAsStringFans(addFan "Liz" "Avatar" testDatabase))
+--demo 7 =  putStrLn all fans of films directed by "James Cameron"
+--demo 8  = putStrLn all directors & no. of their films that "Liz" is a fan of
+
+--
+--
+-- Your user interface code goes here
+--
+--
